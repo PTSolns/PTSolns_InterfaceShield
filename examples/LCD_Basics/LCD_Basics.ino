a063693f6650b91b21649fed1187660a1b870d0d
PTSolns <115762362+PTSolns@users.noreply.github.com>
2022-10-21 15:35:12 -0600

    Update LCD_Basics.ino

--- examples/LCD_Basics/LCD_Basics.ino
+++ examples/LCD_Basics/LCD_Basics.ino
@@ -1,5 +1,5 @@
 // EXAMPLE: LCD Basics
-// Last update: Oct 18, 2022
+// Last update: Oct 21, 2022
 // contact@PTSolns.com
 //
 // DESCRIPTION
@@ -8,18 +8,17 @@
 // HARDWARE CONFIGURATION
 // Use Uno R3, Leonardo, Mega, or any other similar uC operating on 5V logic level and has same pinout.
 // Onboard selection pins of the Interface-Shield:
-// - "PWR" connect -> this powers ON the entire shield
-// - "LCD D9" leave unconnected -> this can dim the LCD backlight via digital pin D9. See example "LCD Backlight Dimming" for this feature.
-// - "LCD Pot" connect -> this uses the onboard potentiometer to adjust the LCD backlight brightness. Turn the pot to adjust brightness as desired.
-// - "D2" leave unconnected -> this is used to send interrupt to D2 on uC. See example "Interrupt" for this feature.
+// - "PWR" ........ connect -> powers ON the entire shield
+// - "LCD D9" not connected -> can dim the LCD backlight via digital pin D9. See example "LCD Backlight Dimming" for this feature.
+// - "LCD Pot" .... connect -> uses the onboard potentiometer to adjust the LCD backlight brightness. Turn the pot to adjust brightness as desired.
+// - "D2" ... not connected -> used to send interrupt to D2 on uC. See example "Interrupt" for this feature.
 
-#include <PTSolns_InterfaceShield.h>
+#include "PTSolns_InterfaceShield.h"
 
 Interface interface;
     
 void setup(){
-  interface.begin(0x27);   // Use the default address 0x27 unless you've closed any of A0, A1, A2 on the back of the Interface-Shield. 
-  interface.backlight(ON); // LCD backlight is initially OFF. Here we turn it ON.
+  interface.begin();  // Use the default address by leaving argument blank (0x27). See "IO Expander Address" example for details.
 }
 
 void loop(){
@@ -38,7 +37,9 @@ void loop(){
   delay(2000);
 
   interface.backlight(ON);
+  interface.setCursor(0,0);
   interface.print("...");
+  delay(2000);
 
   // All LCD commands include:
   // interface.setCurser(0,0); // NOTE: The Interface-Shield uses the 1602 LCD.
